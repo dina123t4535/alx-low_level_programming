9f31d1e2a8c2b2860c746f73c527c422f5069d8d
dinafathy <dinafathy185@gmail.com>
2023-07-18 09:23:23 -0700

    D

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main-  A C program
+ *
+ * Description: thats prints with printf function.
+ *
+ * Return: always 0 (succes)
+*/
+
+int(void)
+{
+	int n = 1;
+
+	if (n > 0)
+{
+	printf("n is positive 0\n");
+}
+	if (n = =)
+{
+	printf("n is Zero 0\n");
+}
+	if (n < 0)
+{
+	printf("n is negative 0\n");
+}
+	return (0);
+}
